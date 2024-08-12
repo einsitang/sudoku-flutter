3cdaec545b2f89808a61f63f15cb66c41998c011
Element <my-tangjianbin@163.com>
2024-08-12 22:44:55 +0800

    add firebase

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,5 +1,6 @@
 /// application global constant define here
 class Constant {
+  static const bool enableGoogleFirebase = true;
   static const String packageName = "com.sevlow.app.sudoku";
   static const String githubRepository =
       "https://github.com/einsitang/sudoku-flutter";
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'dart:isolate';
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/sudoku_localizations.dart';
@@ -9,6 +14,8 @@ import 'package:sudoku/page/bootstrap.dart';
 import 'package:sudoku/page/sudoku_game.dart';
 import 'package:sudoku/state/sudoku_state.dart';
 
+import 'constant.dart';
+import 'firebase_options.dart';
 import 'ml/detector.dart';
 
 final Logger log = Logger();
@@ -22,6 +29,35 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // firebase init
+  _firebaseInit() async {
+    if (!Constant.enableGoogleFirebase) {
+      log.i("Google Firebase is disable.");
+      return;
+    }
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    // Flutter Not Catch Error Handler
+    FlutterError.onError = (FlutterErrorDetails details) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(details);
+    };
+    // Flutter Async Error Handler
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+    // Flutter Isolate Context Error Handler
+    Isolate.current.addErrorListener(RawReceivePort((pair) async {
+      final List<dynamic> errorAndStacktrace = pair;
+      await FirebaseCrashlytics.instance.recordError(
+        errorAndStacktrace.first,
+        errorAndStacktrace.last,
+        fatal: true,
+      );
+    }).sendPort);
+  }
+
   // warmed up effect when application build before
   _soundEffectWarmedUp() async {
     await SoundEffect.init();
@@ -34,6 +70,7 @@ class MyApp extends StatelessWidget {
   }
 
   Future<SudokuState> _loadState() async {
+    await _firebaseInit();
     await _soundEffectWarmedUp();
     await _modelWarmedUp();
     return await SudokuState.resumeFromDB();
